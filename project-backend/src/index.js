22da36c94531c6aa74df1714de57e247b8ffc7cb
Alejandra-Villalobos <00132820@uca.edu.sv>
2022-06-29 20:08:48 -0600

    Handlers added

--- project-backend/src/index.js
+++ project-backend/src/index.js
@@ -1,5 +1,20 @@
+const Express = require("express");
+const CorsMiddleware = require("cors")
 const { initializeDB } = require("./lib/db/");
+const RequestHandler = require("./lib/handlers/handlers");
 
-initializeDB().then(() => {
-    console.log("DATABASE READY");
+const Api = Express();
+
+Api.use(CorsMiddleware());
+Api.use(Express.json());
+Api.use(Express.urlencoded({ extended: false }));
+Api.use("/api/v1", RequestHandler);
+
+Api.listen(3000, ()=>{
+    console.log("API RUNNING");
+
+    initializeDB().then(() => {
+        console.log("DATABASE READY");
+    });
 });
+
---
+++ project-backend/src/lib/handlers/handlers.js
@@ -0,0 +1,98 @@
+const Express = require("express");
+const { getDBHandler } = require("../db");
+
+const RequestHandler = Express.Router();
+
+RequestHandler.get("/to-dos", async (req, res, next) => {
+    try {
+        const dbHandler = await getDBHandler();
+        const todos = await dbHandler.all("SELECT * FROM todos");
+        await dbHandler.close();
+        if(!todos || !todos.length){
+            return res.status(404).send({ message: "To Dos Not Found" }).end();
+        }
+        res.send({ todos })
+    } catch (error) {
+        res.status(500).send({
+            error: `Error while trying to get the to dos list:`,
+            errorInfo: error.message,
+        });
+    }
+});
+
+RequestHandler.post("/to-dos", async (req, res, next) => {
+    try {
+        const { title, description, isDone: is_done } = req.body;
+        const dbHandler = await getDBHandler();
+        const newTodo = await dbHandler.run(`
+            INSERT INTO todos (title, description, is_done)
+            VALUES (
+                '${title}',
+                '${description}',
+                ${is_done}
+            )
+        `);
+        await dbHandler.close();
+        res.send({ newTodo })
+    } catch (error) {
+        res.status(500).send({
+            error: `Error while trying to create a to do:`,
+            errorInfo: error.message,
+        });
+    }
+});
+
+RequestHandler.patch("/to-dos/:id", async (req, res) => {
+    try {
+        const todoId = req.params.id;
+        const { title, description, isDone: is_done } = req.body;
+        const dbHandler = await getDBHandler();
+        const todoToUpdate = await dbHandler.get(
+            "SELECT * FROM todos WHERE id = ?",
+            todoId
+        );
+        await dbHandler.run(`
+            UPDATE todos 
+            SET title = ?, description = ?, is_done = ?
+            WHERE id = ?`, 
+            title || todoToUpdate.title,
+            description || todoToUpdate.description,
+            is_done !== undefined ? is_done : todoToUpdate.is_done,
+            todoId
+        );
+        
+        const updatedTodo = await dbHandler.get(
+            "SELECT * FROM todos WHERE id = ?",
+            todoId
+          );
+      
+          await dbHandler.close();
+      
+          res.send({ updatedTodo });
+    } catch (error) {
+        res.status(500).send({
+            error: `Error while trying to update a to do:`,
+            errorInfo: error.message,
+        });
+    }
+});
+
+RequestHandler.delete("/to-dos/:id", async (req, res) => {
+    try {
+        const todoId = req.params.id;
+        const dbHandler = await getDBHandler();
+        const deletedTodo = await dbHandler.run(
+            "DELETE FROM todos WHERE id = ?",
+            todoId
+        );
+        await dbHandler.close();
+        res.send({ todoRemoved: { ...deletedTodo } });
+    } catch (error) {
+        res.status(500).send({
+            error: `Error while trying to delete a to do:`,
+            errorInfo: error.message,
+        });
+    }
+});
+
+module.exports = RequestHandler;
